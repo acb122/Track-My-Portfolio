70d5a334ac7978687fd8da1b378590d0ec21a33f
acb122 <t-albar@microsoft.com>
2017-11-13 00:38:13 +0000

    firebase and travis added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyB2GtyWM-Cj3NSU1X_NbwgMQyf8icqfmDM',
+  authDomain: ' track-my-portfolio.firebaseapp.com',
+  databaseURL: 'https://track-my-portfolio.firebaseio.com',
+  projectId: 'track-my-portfolio',
+  storageBucket: 'track-my-portfolio.appspot.com',
+  messagingSenderId: '979970953428'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig )
   ],
   providers: [],
   bootstrap: [AppComponent]
