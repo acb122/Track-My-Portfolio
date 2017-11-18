21b38a29b6a03f92ac6a294a8fd64d99c93480f0
Xander Barnes <xanderbarnes@Xanders-MacBook-Pro.local>
2017-11-18 14:27:25 +0000

    add enviroment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AuthService } from './auth.service';
@@ -17,15 +18,7 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB2GtyWM-Cj3NSU1X_NbwgMQyf8icqfmDM",
-  authDomain: "track-my-portfolio.firebaseapp.com",
-  databaseURL: "https://track-my-portfolio.firebaseio.com",
-  projectId: "track-my-portfolio",
-  storageBucket: "track-my-portfolio.appspot.com",
-  messagingSenderId: "979970953428"
-};
-
+export const firebaseConfig = environment.firebaseConfig
 @NgModule({
   declarations: [
     AppComponent,
