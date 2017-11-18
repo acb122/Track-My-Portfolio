57f66b1856907d5bde95e5dd52ce95dd16d37f19
Xander Barnes <xanderbarnes@Xanders-MacBook-Pro.local>
2017-11-18 01:50:41 +0000

    100%

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -7,8 +7,11 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AboutComponent } from './about/about.component';
 import { Router } from '@angular/router';
+import { defer } from 'q';
 
 let authSubject: Subject<firebase.User>
+const deferred = defer()
+
 describe('AuthService', () => {
   beforeEach(() => {
     authSubject = new Subject<firebase.User>()
@@ -17,12 +20,11 @@ describe('AuthService', () => {
     }
 
     fakeFireBaseAuth.auth.signOut.and.returnValue(Promise.resolve(true));
-
-    fakeFireBaseAuth.auth.signInWithPopup.and.returnValue(Promise.resolve(true));
+    fakeFireBaseAuth.auth.signInWithPopup.and.returnValue(deferred.promise);
 
 
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [RouterTestingModule.withRoutes([{ path: 'dashboard', pathMatch: 'full', redirectTo: '/' }])],
 
       providers: [
         AuthService,
@@ -34,11 +36,18 @@ describe('AuthService', () => {
     expect(service).toBeTruthy()
   }));
 
+
+
+  it('should not able to login', inject([AuthService], (service: AuthService) => {
+    deferred.reject('t')
+    service.login()
+  }));
+
   it('should be able to login', inject([AuthService], (service: AuthService) => {
     const router = TestBed.get(Router)
+    deferred.resolve('t');
     const navigateSpy = spyOn(router, 'navigate')
     service.login()
-
   }));
 
   it('should be able to logout', inject([AuthService], (service: AuthService) => {
