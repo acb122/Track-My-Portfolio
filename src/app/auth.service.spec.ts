2a9a39ba65f27d243dca41c831176703d944b2ee
Xander Barnes <xanderbarnes@Xanders-MacBook-Pro.local>
2017-11-18 00:58:11 +0000

    improve tests

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -5,20 +5,24 @@ import * as firebase from 'firebase/app'
 import { Subject } from 'rxjs/Subject'
 import { AngularFireAuth } from 'angularfire2/auth';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AboutComponent } from './about/about.component';
+import { Router } from '@angular/router';
 
 let authSubject: Subject<firebase.User>
 describe('AuthService', () => {
   beforeEach(() => {
     authSubject = new Subject<firebase.User>()
     const fakeFireBaseAuth = {
-      authState: authSubject,
       auth: jasmine.createSpyObj('auth', ['signOut', 'signInWithPopup'])
     }
 
     fakeFireBaseAuth.auth.signOut.and.returnValue(Promise.resolve(true));
 
+    fakeFireBaseAuth.auth.signInWithPopup.and.returnValue(Promise.resolve(true));
+
+
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule.withRoutes([])],
 
       providers: [
         AuthService,
@@ -28,5 +32,19 @@ describe('AuthService', () => {
 
   it('should be created', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy()
+  }));
+
+  it('should be able to login', inject([AuthService], (service: AuthService) => {
+    const router = TestBed.get(Router)
+    const navigateSpy = spyOn(router, 'navigate')
+    service.login()
+
+  }));
+
+  it('should be able to logout', inject([AuthService], (service: AuthService) => {
+    const router = TestBed.get(Router)
+    const navigateSpy = spyOn(router, 'navigate')
+    service.logout()
+    expect(navigateSpy).toHaveBeenCalledWith(['/'])
   }));
 });
